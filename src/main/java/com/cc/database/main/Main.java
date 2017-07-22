132b53a759caf5837748b334a6e2e077bc067084
wpy <3285840393@qq.com>
2017-07-22 19:50:08 +0800

    测试1

---
+++ src/main/java/com/cc/database/main/Main.java
@@ -0,0 +1,32 @@
+package com.cc.database.main;
+
+import com.cc.database.sql.Initializer;
+import com.cc.database.sql.sql_analysis;
+
+import java.util.Scanner;
+
+/**
+ * Created by wangpeiyu on 2017/7/22.
+ */
+public class Main {
+
+    public static  void main(String args[]){
+
+        sql_analysis  analysis = new sql_analysis();
+
+        Initializer initializer = new Initializer(analysis.getFilter());
+
+        System.out.println("Data is initing ....");
+
+        initializer.processData();
+
+        Scanner scanner = new Scanner(System.in);
+
+        if(scanner.hasNextLine())
+        {
+            String exce = scanner.nextLine();
+            analysis.execute(exce);
+        }
+
+    }
+}
