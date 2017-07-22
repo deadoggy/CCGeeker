5518dac96fe0a4883880307ee502ee80bd0e74da
deadoggy <deadoggy@aliyun.com>
2017-07-22 16:13:18 +0800

    modified:   .idea/workspace.xml 	new file:   src/main/java/com/cc/database/Disk/DataOperator.java 	new file:   src/main/java/com/cc/database/Disk/Model.java 	new file:   src/main/java/com/cc/database/Disk/Operator.java

---
+++ src/main/java/com/cc/database/Disk/Model.java
@@ -0,0 +1,19 @@
+package com.cc.database.Disk;
+
+/**
+ * Created by deadoggy on 17-7-22.
+ */
+public class Model {
+
+    public String Name;
+
+    public String Phone;
+
+    public String Email;
+
+    public String Company;
+
+    public String Department;
+
+    public String Position;
+}
